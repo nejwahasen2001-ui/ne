a9d12ba69f71a91920c266fa438b4081e5b7954a
nejwahasen2001-ui <nejwahasen2001@gmail.com>
2026-05-12 00:38:37 +0300

    Update database connection details in config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$host = 'localhost';
-$dbname = 'nejwa_portfolio';
-$user = 'root';
-$pass = '';  // Laragon default is empty
+$host = 'sql102.infinityfree.com';        // from your screenshot
+$dbname = 'if0_41893575_nejwaportfolio';   // from your screenshot (the database you created)
+$user = 'if0_41893575';                   // from your screenshot
+$pass = 'PASTE_YOUR_MYSQL_PASSWORD_HERE'; // the password you set when creating the database
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
