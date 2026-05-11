d10d5f2828469dfd028f2b0a8a3b4274d58efec4
nejwahasen2001-ui <nejwahasen2001@gmail.com>
2026-05-12 00:41:11 +0300

    Update database connection parameters in config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$host = 'sql102.infinityfree.com';        // from your screenshot
-$dbname = 'if0_41893575_nejwaportfolio';   // from your screenshot (the database you created)
-$user = 'if0_41893575';                   // from your screenshot
-$pass = 'PASTE_YOUR_MYSQL_PASSWORD_HERE'; // the password you set when creating the database
+$host = 'sql102.infinityfree.com';       
+$dbname = 'if0_41893575_nejwaportfolio'; 
+$user = 'if0_41893575';                   
+$pass = 'PASTE_YOUR_MYSQL_PASSWORD_HERE'; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
